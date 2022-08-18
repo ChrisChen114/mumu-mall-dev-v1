bb634c1b49769b96da32efe036cbfee55688606b
ChrisChen114 <chen.qianfu114@gmail.com>
2022-08-18 02:15:11 -0700

    first-commit

---
+++ src/main/java/com/imooc/malldevv1/ImoocMallDevV1Application.java
@@ -0,0 +1,13 @@
+package com.imooc.malldevv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocMallDevV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocMallDevV1Application.class, args);
+    }
+
+}
